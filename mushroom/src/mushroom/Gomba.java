eb04f59158910545b7153b4d601e28e183e9dc8d
unknown <feymiso@gmail.com>
2018-01-16 18:04:45 +0100

    a harom osztaly

---
+++ mushroom/src/mushroom/Gomba.java
@@ -0,0 +1,17 @@
+package mushroom;
+
+import javax.lang.model.type.PrimitiveType;
+
+public class Gomba {
+
+	private double meret;
+	private double x;
+	private double y;
+	private int fertozott[][];
+	
+	public Gomba(double size, double x, double y) {
+		meret=size;
+		this.x=x;
+		this.y=y;
+	}
+}
---
+++ mushroom/src/mushroom/TestCase.java
@@ -0,0 +1,16 @@
+package mushroom;
+
+
+public class TestCase {
+	private int amount;
+	private double[] sizes;
+	private double[][] affected;
+	
+	
+	/*private void gombaGenerate(int darab, double[] meretek, double[] xek, double[] yok, String[] nevek) {
+		for(int i=0; i<darab;i++) {
+			Gomba nevek[i] = new Gomba(meretek[i], xek[i], yok[i]);
+		}
+	}	
+	*/
+}
